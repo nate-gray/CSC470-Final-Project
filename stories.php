efd4980920e3363a9d38a61f01037c05cf93a4e4
n.gray <n.gray@tsc-1130>
2018-04-22 15:27:27 -0400

    Fixes https://github.com/ngray311/CSC470-Final-Project/issues/3

--- stories.php
+++ stories.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION['username'])) {
     print '<table cellpadding="2" cellspacing="2"><tr><th>Name</th><th>Last Modified</th></tr>';
     foreach($contents as $item) {
         if((is_file($dir . '/' . $item)) AND ( substr($item, 0, 1) != '.') AND ($item != 'books.csv')) {
-            $last_modified = date('F j, Y', filemtime($dir . '/' . $item));
+            $last_modified = date('F j, Y g:i:s a', filemtime($dir . '/' . $item));
             print "<tr><td>$item</td><td>$last_modified</td></tr>";
         } 
     }
--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,7 @@
 <?php
 ob_start();
 session_start();
+date_default_timezone_set('America/Chicago');
 $logged_in = false;
 $is_admin = false;
 
